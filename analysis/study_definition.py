bbacff21f253a8dec841fdeb12fcf553528a56ad
doro-s <dorothee.schneider@ons.gov.uk>
2022-03-25 11:41:59 +0000

    test files

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,28 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+# import required functions from cohortextractor package
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  
+     
+
+from codelists import *
 
 
 study = StudyDefinition(
     default_expectations={
+        # apply to all subsequently defined variables; override in return_expectations in var extractor function when necessary
+        # yet more comment
         "date": {"earliest": "1900-01-01", "latest": "today"},
         "rate": "uniform",
         "incidence": 0.5,
     },
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
+    ),
+
+     age=patients.age_as_of(
+        "2019-09-01",
+        return_expectations={
+             # every var needs return_expectations argument (except population), defining distribution, more comment
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
     ),
 )

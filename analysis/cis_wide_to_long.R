88e93f84d3e06c498ad3a2118fbec1623286daec
llrnzi <l.lrnzi.100@gmail.com>
2022-06-14 11:47:23 +0100

    new branch, changing cis from wide to long format

---
+++ analysis/cis_wide_to_long.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+cis_wide <- read_csv('D:/MH_pandemic/output/input_cis_wide.csv')
+
+cis_long <- cis_wide %>%
+  select(patient_id, matches('cis\\_visit\\_date\\_\\d+')) %>% 
+  pivot_longer(cols = -patient_id,
+               names_to = c(NA, 'visit_number'),
+               names_pattern = '^(.*)_(\\d+)',
+               values_to = 'visit_date',
+               values_drop_na = TRUE) %>%
+  arrange(patient_id, visit_number)
+
+write_csv(cis_long, 'D:/MH_pandemic/output/input_cis_long.csv')

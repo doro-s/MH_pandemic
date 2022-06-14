1b3f48492b0db054224ecc6a0bbb7399e64df8f6
Lisa Hopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2022-06-14 12:57:37 +0100

    feat: changing to relative directory names

--- analysis/cis_wide_to_long.R
+++ analysis/cis_wide_to_long.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-cis_wide <- read_csv('D:/MH_pandemic/output/input_cis_wide.csv')
+cis_wide <- read_csv('output/input_cis_wide.csv')
 
 cis_long <- cis_wide %>%
   select(patient_id, matches('cis\\_visit\\_date\\_\\d+')) %>% 
@@ -11,4 +11,4 @@ cis_long <- cis_wide %>%
                values_drop_na = TRUE) %>%
   arrange(patient_id, visit_number)
 
-write_csv(cis_long, 'D:/MH_pandemic/output/input_cis_long.csv')
+write_csv(cis_long, 'output/input_cis_long.csv')

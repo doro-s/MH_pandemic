620f64fcc4215cff4bbd8d568e7552349d2a9510
schofr2 <94460430+schofr2@users.noreply.github.com>
2023-04-20 14:37:38 +0100

    cleaning code 14 and 15

--- analysis/14_testing_waves.R
+++ analysis/14_testing_waves.R
@@ -1,3 +1,11 @@
+# This code tests the interaction between time(time from strat date to index date)#
+# We use spline(time) to do that                                                  #
+#                     COX PROPORTIONAL HAZARD RATIO                               #
+#                                                                                 #
+# This is necessary as results from the Sch.Residuals showed that the             #
+#  assumption has not been met. first we calculate estimates using                #
+#  coxph( Surv()....then we test significance using Anova()                       #
+
 library(tidyverse)
 library(data.table)
 library(survival)
@@ -7,22 +15,20 @@ library(splines)
 library(gridExtra)
 library(car)
 
-#rm(list=ls())
-
 options(datatable.fread.datatable=FALSE)
 
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #setwd('../')
 
 ###########################################################################
-# Load data 
-# create new variable for waves 
+# Set study start date and chnage it to numeric 
+###########################################################################
 
-#wave dates
+start_date <- as.Date("2020/01/24") 
+start_date_numeric <- as.numeric(start_date) #this is converted into days
 
-# alphanad_before <- "2020-11-16" to "2021-05-16"
-# delta <- "2021-05-17" to "2021-12-19"
-# omnicrom <- "2021-12-20" to "2022-10-19" 
+###########################################################################
+# Load data & create new variable for waves 
 ###########################################################################
 
 incidence <- fread('output/incidence_t.csv') %>% 
@@ -41,25 +47,14 @@ prevalence <- fread('output/prevalence_t.csv') %>%
                      index_date >="2021-12-20" ~ "omnicron"))
 
 
+
+
+
 ###########################################################################
-#     Test significance of time (spline) to explore the interaction
-#             between time and the exposed variable
-#         # incidence 
-# convert index date as numeric 
-#     Change index date to numeric so that we can use it as spline
-# numbers relative to the start date 2020-01-24
+# Summary check on dates - needed to check all dates are within range
 ###########################################################################
 
-start_date <- as.Date("2020/01/24") 
-print(start_date)
-
-start_date_numeric <- as.numeric(start_date) #this is converted into days
-print(start_date_numeric)
-
-# summary check of index dates
-
-# INCIDENCE 
-
+# Chnage index_dates to numeric
 incidence$index_numeric <- as.numeric(incidence$date_positive) 
 incidence$index_time_to_start_date <- incidence$index_numeric - start_date_numeric  
 
@@ -89,217 +84,167 @@ summary(prevalence$index_numeric)
 print('summary(prevalence$index_time_to_start_date)')
 summary(prevalence$index_time_to_start_date)
 
-
 print('NAs number')
 prevalence %>% filter(is.na(index_date)) %>% nrow()
 
-######################################################################################################
-######################################################################################################
-#
-#         Testing the interaction between exposure and time (time as a spline)
-#
-######################################################################################################
-######################################################################################################
+###########################################################################
+# Test significance of interaction between time(spline) & exposed variable
+###########################################################################
 
 # INCIDENCE 
 
-print('1. Incidence index numeric model - spline')
-incidence_with_spline <- coxph(Surv(t,mh_outcome)~ exposed*ns(index_time_to_start_date, df = 2, 
-                                               Boundary.knots = c(quantile(index_time_to_start_date,0.1),
-                                                                  quantile(index_time_to_start_date, 0.9))), 
-                               data = incidence)
-print(incidence_with_spline)
-TIDY_WITH_SPLINE <-tidy(incidence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-print(TIDY_WITH_SPLINE)
+spline_m1 <- coxph(Surv(t,mh_outcome)~ exposed*ns(index_time_to_start_date, df = 2, 
+                                                  Boundary.knots = c(quantile(index_time_to_start_date,0.1),
+                                                                     quantile(index_time_to_start_date, 0.9))), 
+                   data = incidence)
 
-print('1a. Anova incidince - spline')
-anova_incidence_with_spline <- Anova(incidence_with_spline, row.names = TRUE)
-anova_TIDY_WITH_SPLINE <-tidy(anova_incidence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-print(anova_incidence_with_spline)
+spline_m2 <- coxph(Surv(t,mh_outcome)~ exposed*ns(index_time_to_start_date, df = 2, 
+                                                  Boundary.knots = c(quantile(index_time_to_start_date,0.1),
+                                                                     quantile(index_time_to_start_date, 0.9))) + 
+                     sex + ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))), 
+                   data = incidence)
 
-#a1 <- Anova(incidence_with_spline, row.names = TRUE)
-#a2 <-tidy(a1, conf.int=TRUE,exponentiate = TRUE) 
-#print(a2)
-
-#write_csv(TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_INCIDENCE_SPLINE_TIME.csv')
-write_csv(anova_TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_INCIDENCE_ANOVA.csv')
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-print('2. Incidence index numeric model - spline + AGE +SEX')
-incidence_with_spline <- coxph(Surv(t,mh_outcome)~ exposed*ns(index_time_to_start_date, df = 2, 
-                                                              Boundary.knots = c(quantile(index_time_to_start_date,0.1),
-                                                                                 quantile(index_time_to_start_date, 0.9))) + sex + ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))), 
-                               data = incidence)
-print(incidence_with_spline)
-TIDY_WITH_SPLINE <-tidy(incidence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-print(TIDY_WITH_SPLINE)
-
-print('2a. Anova incidince - spline')
-anova_incidence_with_spline <- Anova(incidence_with_spline, row.names = TRUE)
-anova_TIDY_WITH_SPLINE <-tidy(anova_incidence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-print(anova_incidence_with_spline)
-
-#write_csv(TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_INCIDENCE_SPLINE_TIME_sex_age.csv')
-write_csv(anova_TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_INCIDENCE_ANOVA_sex_age.csv')
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-print('3. Incidence index numeric - spline + fully adjusted')
-incidence_with_spline <- coxph(Surv(t,mh_outcome) ~ exposed*ns(index_time_to_start_date, df = 2, 
-                                                              Boundary.knots = c(quantile(index_time_to_start_date,0.1),
-                                                                                 quantile(index_time_to_start_date, 0.9))) + 
-                                 ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))) + 
-                                 alcohol + 
-                                 obese_binary_flag + 
-                                 cancer + 
-                                 digestive_disorder + 
-                                 hiv_aids + 
-                                 kidney_disorder + 
-                                 respiratory_disorder + 
-                                 metabolic_disorder + 
-                                 sex + 
-                                 ethnicity + 
-                                 region + 
-                                 hhsize + 
-                                 work_status_new + CVD + 
-                                 musculoskeletal + 
-                                 neurological + 
-                                 mental_behavioural_disorder, 
-                               data = incidence)
+spline_m3 <- coxph(Surv(t,mh_outcome) ~ exposed*ns(index_time_to_start_date, df = 2, 
+                                                   Boundary.knots = c(quantile(index_time_to_start_date,0.1),
+                                                                      quantile(index_time_to_start_date, 0.9))) + 
+                     ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))) + 
+                     alcohol + 
+                     obese_binary_flag + 
+                     cancer + 
+                     digestive_disorder + 
+                     hiv_aids + 
+                     kidney_disorder + 
+                     respiratory_disorder + 
+                     metabolic_disorder + 
+                     sex + 
+                     ethnicity + 
+                     region + 
+                     hhsize + 
+                     work_status_new + CVD + 
+                     musculoskeletal + 
+                     neurological + 
+                     mental_behavioural_disorder +
+                     imd + 
+                     rural_urban, 
+                   data = incidence)
 
 
 
-print(incidence_with_spline)
-TIDY_WITH_SPLINE <-tidy(incidence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-print(TIDY_WITH_SPLINE)
+tidy_spline_m1 <-tidy(spline_m1, conf.int=TRUE,exponentiate = TRUE) 
+tidy_spline_m2 <-tidy(spline_m2, conf.int=TRUE,exponentiate = TRUE) 
+tidy_spline_m3 <-tidy(spline_m3, conf.int=TRUE,exponentiate = TRUE) 
 
-print('3a. IMPORTANT - Anova incidince WITH spline')
-anova_incidence_with_spline <- Anova(incidence_with_spline, row.names = TRUE)
-anova_TIDY_WITH_SPLINE <-tidy(anova_incidence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-print(anova_incidence_with_spline)
-
-
-#write_csv(TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_INCIDENCE_SPLINE_TIME_fully_adjusted.csv')
-write_csv(anova_TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_INCIDENCE_ANOVA_fully_adjusted.csv')
+#write_csv(tidy_spline_m1, 'output/99_anova_exposed_spline_time_unadj_incidence.csv')
+#write_csv(tidy_spline_m2, 'output/99_anova_exposed_spline_time_sexage_incidence.csv')
+write_csv(tidy_spline_m3, 'output/99_coeff_exposed_spline_time_fulladj_incidence.csv')
 
 
-######################################################################################################
-# PREVALENCE 
+# run anova & save 
+a_m1 <-tidy(Anova(spline_m1, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE)
+a_m2 <-tidy(Anova(spline_m2, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE)
+a_m3 <-tidy(Anova(spline_m3, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE)
 
-# Testing the interaction between exposure and time (time as a spline)
-#
-######################################################################################################
+a_m1$model <- "unadj"
+a_m2$model <- "sex & age"
+a_m3$model <- "fully adjusted"
 
-print('1. prevalence index numeric WITHOUT spline')
-prevalence_without_spline <- coxph(Surv(t,mh_outcome)~ exposed*index_time_to_start_date ,data = prevalence)
-print(prevalence_without_spline)
+avova_table <- rbind(a_m1, a_m2, a_m3)
 
-print('1a. Anova prevalence WITHOUT spline')
-anova_prevalence_without_spline <- Anova(prevalence_without_spline)
-print(anova_prevalence_without_spline)
-
-print('2. IMPORTANT - prevalence index numeric with spline')
-prevalence_with_spline <- coxph(Surv(t,mh_outcome)~ exposed*ns(index_time_to_start_date, df = 2, 
-                                                               Boundary.knots = c(quantile(index_time_to_start_date,0.1),
-                                                                                  quantile(index_time_to_start_date, 0.9))), data = prevalence)
-
-print(prevalence_with_spline)
-
-TIDY_WITH_SPLINE <-tidy(prevalence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-
-print(TIDY_WITH_SPLINE)
-print('2a. IMPORTANT - Anova prevalence WITH spline')
-anova_prevalence_with_spline <- Anova(prevalence_with_spline, row.names = TRUE)
-anova_TIDY_WITH_SPLINE <-tidy(anova_prevalence_with_spline, conf.int=TRUE,exponentiate = TRUE) 
-
-print(anova_TIDY_WITH_SPLINE)
-#write_csv(TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_PREV_SPLINE_TIME.csv')
-write_csv(anova_TIDY_WITH_SPLINE, 'output/99_TEMPORARY_COX_PREV_ANOVA.csv')
+# save anova table for all models
+write_csv(avova_table, 'output/99_anova_exposed_spline_time_interactions.csv')
 
 
 
+#############################    PREVALENCE    #################################
+
+spline_p1 <- coxph(Surv(t,mh_outcome)~ exposed*ns(index_time_to_start_date, df = 2, 
+                                                  Boundary.knots = c(quantile(index_time_to_start_date,0.1),
+                                                                     quantile(index_time_to_start_date, 0.9))) + 
+                     ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))) + 
+                     alcohol + 
+                     obese_binary_flag + 
+                     cancer + 
+                     digestive_disorder + 
+                     hiv_aids + 
+                     kidney_disorder + 
+                     respiratory_disorder + 
+                     metabolic_disorder + 
+                     sex + 
+                     ethnicity + 
+                     region + 
+                     hhsize + 
+                     work_status_new + CVD + 
+                     musculoskeletal + 
+                     neurological + 
+                     mental_behavioural_disorder +
+                     imd + 
+                     rural_urban, data = prevalence)
 
 
+tidy_spline_p1 <-tidy(spline_p1, conf.int=TRUE,exponentiate = TRUE) 
+write_csv(tidy_spline_p1, 'output/99_coeff_exposed_spline_time_fulladj_prev.csv')
+
+# anova
+a_spline_p1 <-tidy(Anova(spline_p1, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE) 
+
+write_csv(a_spline_p1, 'output/99_anova_exposed_spline_time_fulladj_prev.csv')
 
 
-######################################################################################################
-######################################################################################################
-#
-#         Testing the interaction between exposure and WAVES
-#
-######################################################################################################
-######################################################################################################
+###########################################################################
+# Test significance of interaction between WAVES & exposed variable
+###########################################################################
+# The above analysis showed only the incidence group to have a statistically
+#  significant relationship between time and exposed category. Therefore we 
+#  further test the effects of waves. This is done in a same way, first we 
+#  calculate estimates using coxph( Surv().... then we test significance using
+#  Anova()
 
-int_1 <- coxph(Surv(t,mh_outcome)~ exposed*waves, data = incidence)
-tidy_table <-tidy(int_1, conf.int=TRUE,exponentiate = TRUE) 
-print('INTERACTION WITH WAVES VARIABLE ANOVA')
-anova <- Anova(int_1, row.names = TRUE)
-anova_tidy <-tidy(anova, conf.int=TRUE, exponentiate = TRUE) 
-print(anova_tidy)
+m1 <- coxph(Surv(t,mh_outcome)~ exposed*waves, data = incidence)
 
-#save
-write_csv(anova_tidy, 'output/99_anova_waves.csv')
+m2 <- coxph(Surv(t,mh_outcome)~ exposed*waves + 
+              sex + 
+              ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))), 
+            data = incidence)
 
+m3 <- coxph(Surv(t,mh_outcome)~ exposed*waves + 
+              ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))) + 
+              alcohol + 
+              obese_binary_flag + 
+              cancer + 
+              digestive_disorder + 
+              hiv_aids + 
+              kidney_disorder + 
+              respiratory_disorder + 
+              metabolic_disorder + 
+              sex + 
+              ethnicity + 
+              region + 
+              hhsize + 
+              work_status_new + 
+              CVD + 
+              musculoskeletal + 
+              neurological + 
+              mental_behavioural_disorder +
+              imd + 
+              rural_urban, 
+            data = incidence)
 
+# save cox models of interaction - only save fully adjusted for coeff.
+m3_tidy <-tidy(m3, conf.int=TRUE,exponentiate = TRUE) 
 
-print('INTERACTION WITH WAVES VARIABLE- age and sex')
+write_csv(m3_tidy, 'output/99_coefficients_for_waves_incidence.csv')
 
-int_1 <- coxph(Surv(t,mh_outcome)~ exposed*waves + 
-                 sex + 
-                 ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))), 
-               data = incidence)
-tidy_table <-tidy(int_1, conf.int=TRUE,exponentiate = TRUE) 
-print('INTERACTION WITH WAVES VARIABLE ANOVA')
-anova <- Anova(int_1, row.names = TRUE)
-anova_tidy <-tidy(anova, conf.int=TRUE, exponentiate = TRUE) 
-print(anova_tidy)
+# run anova & save 
+a_m1 <-tidy(Anova(m1, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE)
+a_m2 <-tidy(Anova(m2, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE)
+a_m3 <-tidy(Anova(m3, row.names = TRUE), conf.int=TRUE,exponentiate = TRUE)
 
-#save
-write_csv(anova_tidy, 'output/99_anova_waves_sex_age.csv')
+a_m1$model <- "unadj"
+a_m2$model <- "sex & age"
+a_m3$model <- "fully adjusted"
 
+avova_table <- rbind(a_m1, a_m2, a_m3)
 
-
-
-
-print('INTERACTION WITH WAVES VARIABLE - fully adjusted')
-int_1 <- coxph(Surv(t,mh_outcome)~ exposed*waves + 
-                 ns(age, df = 2, Boundary.knots = c(quantile(age,0.1), quantile(age, 0.9))) + 
-                 alcohol + 
-                 obese_binary_flag + 
-                 cancer + 
-                 digestive_disorder + 
-                 hiv_aids + 
-                 kidney_disorder + 
-                 respiratory_disorder + 
-                 metabolic_disorder + 
-                 sex + 
-                 ethnicity + 
-                 region + 
-                 hhsize + 
-                 work_status_new + 
-                 CVD + 
-                 musculoskeletal + 
-                 neurological + 
-                 mental_behavioural_disorder, 
-               data = incidence)
-tidy_table <-tidy(int_1, conf.int=TRUE,exponentiate = TRUE) 
-print('INTERACTION WITH WAVES VARIABLE ANOVA')
-anova <- Anova(int_1, row.names = TRUE)
-anova_tidy <-tidy(anova, conf.int=TRUE, exponentiate = TRUE) 
-print(anova_tidy)
-
-#save
-write_csv(anova_tidy, 'output/99_anova_waves_fully adjusted.csv')
-write_csv(tidy_table, 'output/99_coefficients_for_waves_incidence.csv')
-
-
-######################################################################################################
-
-
-
-
-
-
-
-
+# save anova table for all models
+write_csv(avova_table, 'output/99_anova_waves_time_interaction.csv')
 

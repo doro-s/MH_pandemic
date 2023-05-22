7dad95ee44c55de71d22a31a46884a61b9488c6b
schofr2 <94460430+schofr2@users.noreply.github.com>
2023-05-22 09:45:13 +0100

    changing matches from 4 to 3

--- analysis/6_create_controls.R
+++ analysis/6_create_controls.R
@@ -213,8 +213,8 @@ match_exposed_to_controls <- function(exposed, control, N){
 
 
 # Run matching
-incidence_group <- match_exposed_to_controls(incidence_exposed, incidence_control, 4)
-prevalence_group <- match_exposed_to_controls(prevalence_exposed, prevalence_control, 4)
+incidence_group <- match_exposed_to_controls(incidence_exposed, incidence_control, 3)
+prevalence_group <- match_exposed_to_controls(prevalence_exposed, prevalence_control, 3)
 
 print('total size of post matched incidence population')
 nrow(incidence_group)
